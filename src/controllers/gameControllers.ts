09edfd9efc7e11bdcc4cc8a76bbb9e481d512a15
IzaFreire <kariny.frei.2000@gmail.com>
2023-04-09 07:59:17 -0300

    feat: game controller

--- src/controllers/gameControllers.ts
+++ src/controllers/gameControllers.ts
@@ -1,5 +1,31 @@
+import { Request, Response } from 'express';
+import { GameService } from '../services/games.service.js';
 
+const gameService = new GameService();
 
+async function createGames(req: Request, res: Response): Promise<void> {
+  try {
+    const game = await gameService.createGame(req.body);
+    res.status(201).send('Game created successfully!');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+async function allGames(req: Request, res: Response): Promise<void> {
+  try {
+    const games = await gameService.getGames();
+    res.send(games);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export default {
+    createGames,
+    allGames
+
+}
 
 
 
