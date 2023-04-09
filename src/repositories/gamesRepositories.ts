f11aff73c64ead0fca11ce0fe0d7637cdb353fc6
IzaFreire <kariny.frei.2000@gmail.com>
2023-04-09 08:05:33 -0300

    feat: game repositories and schemas

---
+++ src/repositories/gamesRepositories.ts
@@ -0,0 +1,30 @@
+import { Game } from '../schemas/GameSchemas.js';
+import { connectionDB } from '../config/database.js';
+
+export class GameRepository {
+  async createGame(game: Game): Promise<Game> {
+    const { name, image, stockTotal, pricePerDay } = game;
+
+    const result = await connectionDB.query(
+      `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4) RETURNING *`,
+      [name, image, stockTotal, pricePerDay]
+    );
+
+    return result.rows[0];
+  }
+
+  async getGames(): Promise<Game[]> {
+    const result = await connectionDB.query(`SELECT * FROM games`);
+    return result.rows;
+  }
+
+  async getGameByName(name: string): Promise<Game | null> {
+    const result = await connectionDB.query(`SELECT * FROM games WHERE name=$1`, [name]);
+
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    return result.rows[0];
+  }
+}
--- src/schemas/GameSchemas.ts
+++ src/schemas/GameSchemas.ts
@@ -1,1 +1,15 @@
+import Joi from "joi";
 
+export const GameSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    image: Joi.string().min(3).required(),
+    stockTotal: Joi.number().required(),
+    pricePerDay: Joi.number().required()
+});
+
+export interface Game {
+  name: string;
+  image: string;
+  stockTotal: number;
+  pricePerDay: number;
+}
